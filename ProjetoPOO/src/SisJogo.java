1e5c18db0e927aefa1f9714b41470546ce5994e4
Gabriel-Ribeiro-Dias <51831189+Gabriel-Ribeiro-Dias@users.noreply.github.com>
2019-08-06 11:38:48 -0300

    Update SisJogo.java

--- ProjetoPOO/src/SisJogo.java
+++ ProjetoPOO/src/SisJogo.java
@@ -1,4 +1,29 @@
+package bancoMobiliario;
 
-public class SisJogo {
+import java.util.ArrayList;
 
+public class sisJogo {
+	
+	ArrayList<Jogador> jogadores = new ArrayList<Jogador>();
+	//ArrayList<Propriedades> propriedades = new ArrayList<Propriedades>();
+	//ArrayList<SorteOuReves> cartas = new ArrayList<SorteOuReves>();
+	
+	public void gravaJogador(Jogador o) {
+		jogadores.add(o);
+	}
+	
+	public void removeJogador(Jogador o) {
+		jogadores.remove(o);
+	}
+	
+	public ArrayList<Jogador> getJogadores(){
+		return jogadores;
+	}
+	
+	/**
+	 * public ArrayList<Propriedades> generateBoard(){
+	 * }
+	 */
+	
+	
 }
